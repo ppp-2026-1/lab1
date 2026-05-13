c7fb1156f3ba663d9e4eac962b63cd3a058a5925
Fabiola Souza Fernandes Pereira <fabiola.pereira@ufu.br>
2026-05-13 11:24:53 -0300

    Add Crow class implementing Bird interface

---
+++ interfacesegregation/incorrect/Crow.java
@@ -0,0 +1,20 @@
+package interfacesegregation.incorrect;
+
+public class Crow implements Bird{
+
+    @Override
+    public void eat() {
+        
+    }
+
+    @Override
+    public void sleep() {
+        
+    }
+
+    @Override
+    public void fly() {
+        
+    }
+    
+}
