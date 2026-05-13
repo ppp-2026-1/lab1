cd448c0640ed5b7fd8c2652d3a2dfd1379984942
Fabiola Souza Fernandes Pereira <fabiola.pereira@ufu.br>
2026-05-13 11:16:27 -0300

    Add GeometryOperation class for area calculation

---
+++ openclosed/incorrect/GeometryOperation.java
@@ -0,0 +1,14 @@
+package openclosed.incorrect;
+
+public class GeometryOperation {
+    
+    public int getArea(Object object) {
+        if(object instanceof Rectangle) {
+            Rectangle rectangle = (Rectangle) object;
+            return rectangle.getHeight() * rectangle.getWidth();
+        } else {
+            Circle circle = (Circle) object;
+            return (int) (Math.PI * circle.getRadius() * circle.getRadius());
+        }
+    }
+}
